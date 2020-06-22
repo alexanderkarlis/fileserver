09b1daa5e23d0a541d00f3864e464b01f63d83dd
Alexander Karlis <alexander.karlis@reactivecore.com>
2020-06-22 09:41:01 -0400

    more rust wasm updates

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@ mod utils;
 use std::io;
 use std::path::Path;
 use std::fs;
-use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -23,29 +23,30 @@ pub fn greet(name: &str) {
     alert(&greeting);
 }
 
-pub fn visit_dirs(dir: &Path, hash_map: <HashMap>, counter: usize) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            let counter_new = counter + 1;
-            if path.is_dir() {
-                visit_dirs(&path, counter_new)?;
-            } else {
-                let file = format!("{}{:#?}", "\t".repeat(counter_new), &entry.path());
-                println!("{}", file);
-            }
-        }
-    }
-    Ok(())
+// fn wrapper() -> String {
+
+//     console::log(&list_dirs());
+// }
+
+#[wasm_bindgen]
+pub fn list_dirs() -> String {
+    let dir_path = "../";
+    let dir = Path::new(dir_path);
+    println!("checking path {:#?}", dir_path);
+    let path_list = &mut Vec::new();
+    let spacing: usize = 0;
+    let dirs_result = check_dirs(&dir, path_list, spacing);
+    println!("{:#?}", dirs_result);
+    
+    let all_dirs = match dirs_result {
+        Ok(dirs) => serde_json::to_string(&dirs).unwrap(),
+        _ => serde_json::to_string(&path_list).unwrap()
+    };
+    let logged = format!("Checking path, {:#?}!", dir_path);
+    console::log_1(&logged.into());
+    all_dirs
 }
 
-use std::io;
-use std::path::Path;
-use std::fs;
-// use std::collections::HashMap;
-// use walkdir::{DirEntry}
-// DirEntry.
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -53,7 +54,7 @@ use std::fs;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 
-fn visit_dirs<'a>(dir: &Path, path_list: &'a mut Vec<String>, counter: usize) -> io::Result<&'a mut Vec<String>> {
+fn check_dirs<'a>(dir: &Path, path_list: &'a mut Vec<String>, counter: usize) -> io::Result<&'a mut Vec<String>> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let counter_new = counter + 1;
@@ -61,7 +62,7 @@ fn visit_dirs<'a>(dir: &Path, path_list: &'a mut Vec<String>, counter: usize) ->
             let path = entry.path();
             println!("{:#?}", path.as_path());
             if path.is_dir() {
-                visit_dirs(&path, path_list, counter_new)?;
+                check_dirs(&path, path_list, counter_new)?;
             } else {
                 let path_str = format!("{}", path.display());
                 path_list.push(path_str);
@@ -71,13 +72,4 @@ fn visit_dirs<'a>(dir: &Path, path_list: &'a mut Vec<String>, counter: usize) ->
         }
     }
     Ok(path_list)
-}
-
-fn main() {
-    let dir = Path::new("/home/alexanderkarlis/test");
-    // let mut path_list: Vec<String> = Vec::new();
-    let path_list = &mut Vec::new();
-    let spacing: usize = 0;
-    let a = visit_dirs(&dir, path_list, spacing);
-    println!("{:#?}", a)
 }
